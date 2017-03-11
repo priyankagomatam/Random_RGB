a38d5797d9597e5f0b49ee134554d2dd2b38af6f
priyankagomatam <priyankagomatam@users.noreply.github.com>
2017-03-11 17:58:41 -0500

    Update send_request.m

--- send_request.m
+++ send_request.m
@@ -1,4 +1,7 @@
 function result = send_request(num, range, col, image_size)
+    if ~isnumeric(num) || rem(n,1) ~= 0 || n < 1
+         error('N should be a valid positive number');
+    end
     if(size(range) ~= 2)
         error('Enter a valid range, specify the min and max values');
     end
@@ -17,21 +20,7 @@ function result = send_request(num, range, col, image_size)
         end       
     end
     % n > 10000 will not work, so send multiple results
-    loop = 1;
-    if(n > 10000) 
-        loop = ceil(n/10000);
-    end
-    remaining = n;
-    for i = 1:loop  
-        quota = check_quota;
-        if(remaining > 10000) 
-            num = remaining - 10000;
-            
-        else
-            remaining = num
-        end
-        remaining = remaining - num;
-        
+    
         if(quota > 0)
             api = ['https://www.random.org/integers/?num=', num2str(num),'&min=1&max=999&col=128&base=10&format=plain&rnd=new'];
             %url = [api 'country/cru/tas/year/USA'];
@@ -41,10 +30,16 @@ function result = send_request(num, range, col, image_size)
 
             catch 
                 warning('Problem using function.  Assigning a value of 0.');
-                result = 'error';
+                [s, stat] = urlread(api);
+                if(stat == 0) 
+                    result = 'error with request';
+                else
+                    result = 'code error';
+                end
             end
 
         else
             result = 'Quota exceeded';
+            backoff;
         end
 end
