cdb33c82d76213fc5adb63fcdd9222d18fc1223d
priyankagomatam <priyankagomatam@users.noreply.github.com>
2017-03-11 17:56:38 -0500

    Update main.m

--- main.m
+++ main.m
@@ -5,7 +5,7 @@ quota = check_quota;
         quota = check_quota;
     end
     for color = 1:3
-        result2 = send_request(9600, [1, 999], 128, [128,128]);
+        result2 = send_request(9600, [1, 999], 128, [128,128]); % Have to automate the split instead of hardcoding requests
         result3 = send_request(6784, [1, 999], 128, [128,128]);
         result = [result2; result3];
         R(:,:,color) = result/1000;
