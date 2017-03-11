9be3ea4658e2e13b7252468ae0d9d0960fbcf966
priyankagomatam <priyankagomatam@users.noreply.github.com>
2017-03-11 18:38:58 -0500

    Update send_request.m

--- send_request.m
+++ send_request.m
@@ -1,4 +1,7 @@
-function result = send_request(num, range, col, image_size)
+function result = send_request(num, range, col)
+    if ~isnumeric(num) || rem(n,1) ~= 0 || n < 1
+         error('N should be a valid positive number');
+    end
     if(size(range) ~= 2)
         error('Enter a valid range, specify the min and max values');
     end
@@ -6,45 +9,38 @@ function result = send_request(num, range, col, image_size)
     n = num;
     min = range(1);
     max = range(2);
-    if(nargin == 3)
-        image = 1;        
-        if(size(range) ~= 2)
-            error('Enter a valid range, specify the row and col size');
-        
-        else
-            image_row = image_size(1);
-            image_col = image_size(2);            
-        end       
-    end
+    
     % n > 10000 will not work, so send multiple results
-    loop = 1;
-    if(n > 10000) 
-        loop = ceil(n/10000);
+    loop=1;
+    if(n>10000)
+        loop = ceil(n/10000); % Number of requests with <10000 requests each
     end
-    remaining = n;
-    for i = 1:loop  
-        quota = check_quota;
-        if(remaining > 10000) 
-            num = remaining - 10000;
-            
-        else
-            remaining = num
+    result = [];
+    for i = 1:loop        
+        num = floor(n/loop); % Divide the number to get chunk size
+        if(i==loop && loop~=1) % If it's the last iteration - useful when chunk sizes are uneven
+            num = n - (loop*num);
         end
-        remaining = remaining - num;
         
         if(quota > 0)
-            api = ['https://www.random.org/integers/?num=', num2str(num),'&min=1&max=999&col=128&base=10&format=plain&rnd=new'];
-            %url = [api 'country/cru/tas/year/USA'];
+            api = ['https://www.random.org/integers/?num=', num2str(num),'&min=', num2str(min), '&max=', num2str(999), '&col=', num2str(col)', '&base=10&format=plain&rnd=new'];
+            
             try
                 S2 = webread(api);
-                result = [str2num(S2)];
+                result = [result, str2num(S2)]; % append the results
 
             catch 
                 warning('Problem using function.  Assigning a value of 0.');
-                result = 'error';
+                [s, stat] = urlread(api);
+                if(stat == 0) 
+                    error('error with request');
+                else
+                    error('code error');
+                end
             end
 
         else
-            result = 'Quota exceeded';
+            warning('Quota exceeded');
+            backoff;
         end
 end
