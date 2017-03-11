727f41de80d70a67e4ee15f833d837b977f9aa89
priyankagomatam <priyankagomatam@users.noreply.github.com>
2017-03-11 18:40:49 -0500

    Update main.m

--- main.m
+++ main.m
@@ -4,13 +4,17 @@ quota = check_quota;
         backoff;
         quota = check_quota;
     end
-    for color = 1:3
-        result2 = send_request(9600, [1, 999], 128, [128,128]); % Have to automate the split instead of hardcoding requests
-        result3 = send_request(6784, [1, 999], 128, [128,128]);
-        result = [result2; result3];
-        R(:,:,color) = result/1000;
-    end 
-        
-%%
+    for color = 1:3      
+        while(quota < 0)
+            backoff;
+            quota = check_quota;
+        end
+        result = send_request(128*128, [1, 999], 1);   
+        res_mat = reshape(result, [128 128]);
+        R(:,:,color) = result/1000; %To get the numbers between 0 and 1. This will change
+                                    % if the range changes. Include 0,1000
+                                    % for white and black colours.
+    end         
+%
 imshow(R)
 
